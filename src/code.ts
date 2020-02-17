e14d489b79f51c7a1d5b258788e6fea82840647a
berhalak <jaroslaw.sadzinski@gmail.com>
2020-02-17 16:37:35 +0100

    Test1

---
+++ src/code.ts
@@ -0,0 +1,31 @@
+type Value<T> = { value(): T }
+type Fun<T> = () => T
+type Prom<T> = PromiseLike<T>
+export type Some<T> = T | Value<T> | Fun<T> | Prom<T> | Value<Prom<T>> | Fun<Prom<T>>
+
+type Mod<T> =
+	T extends Prom<infer R> ? R :
+	T extends Value<Prom<infer R>> ? R :
+	T extends Fun<Prom<infer R>> ? R :
+	T extends Value<infer R> ? R :
+	T extends Fun<infer R> ? R :
+	never;
+
+type IsAsync<T> = T extends Prom<infer R> ? true :
+	T extends Value<Prom<infer R>> ? true :
+	T extends Fun<Prom<infer R>> ? true : false;
+
+type Async<T> = Prom<T> | Value<Prom<T>> | Fun<Prom<T>>;
+type Sync<T> = T | Value<T> | Fun<T>;
+
+type Unwrap<T> = Mod<T> extends never ? T : Mod<T>;
+
+type Return<T> = IsAsync<T> extends true ? PromiseLike<Unwrap<T>> : Unwrap<T>;
+
+type iii = true extends never ? 1 : 2
+type ass = IsAsync<string>
+type www = Unwrap<string>
+type ret = Return<string>
+type ttt = Return<Promise<String>>
+
+
